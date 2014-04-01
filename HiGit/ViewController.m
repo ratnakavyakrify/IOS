15ae22e133ab36c6ec89392ba5664e574bdef1ff
Krishna Reddy Kovvuri <krishnareddykovvuri@Krishnas-Mac-mini-2.local>
2014-04-01 18:09:11 +0530

    adding

--- HiGit/ViewController.m
+++ HiGit/ViewController.m
@@ -19,7 +19,9 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)viewWillAppear:(BOOL)animated{
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
