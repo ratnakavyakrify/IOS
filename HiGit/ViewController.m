db7e180391474606fdc5829a08b5d78c26d39b24
ratnakavyakrify <ratnakavya@krify.net>
2014-09-06 16:03:38 +0530

    git ignore through tool

--- HiGit/ViewController.m
+++ HiGit/ViewController.m
@@ -20,7 +20,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 -(void)viewWillAppear:(BOOL)animated{
-    NSLog(@"hi");
+    NSLog(@"git ignore");
 }
 - (void)didReceiveMemoryWarning
 {
